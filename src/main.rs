50fded497efc8d410ffca6cd19e323ce87554eaa
TISTEA STEFAN VALENTIN <161421675+TisteaStefan@users.noreply.github.com>
2024-06-23 12:12:36 +0000

    change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+use std::path::PathBuf;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: rustwebserver PORT ROOT_FOLDER");
+        return;
+    }
+    let port = &args[1];
+    let root_folder = PathBuf::from(&args[2]);
+    // Print startup information
+    println!("Root folder: {}", folder.display());
+    println!("Server listening on 0.0.0.0:{}", port);
+    
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+ fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {http_request:#?}");
 }
