614b3ac731b061104a26544b8095ac4be8aec8a5
TISTEA STEFAN VALENTIN <161421675+TisteaStefan@users.noreply.github.com>
2024-06-24 12:44:13 +0000

    change

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,7 @@ fn handle_connection(mut stream: TcpStream, path: String) -> io::Result<()>{
             stream.flush().unwrap();
         }
         Err(_e) => {
-            println!("GET 127.0.0.1 {} -> 404 (Not Found)", full_path);
+            println!("GET 127.0.0.1 {} -> 404 (Not Found)", path);
             let response = b"HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n<html>404 Not Found</html>";
             stream.write_all(response).unwrap();
             stream.flush().unwrap();
